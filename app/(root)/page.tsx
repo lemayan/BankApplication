a4aed3866b2df142e9c693a918d7df6f3050347e
lemayan <lemayanleleina@gmail.com>
2025-05-29 10:50:44 +0300

    Update on the DoughnutChart & AnimatedCounter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,38 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Ivy'}
   return (
-    <div>Home</div>
+    <section className='home'>
+
+      <div className='home-content'>
+         <header className='home-header'>
+        <HeaderBox
+
+        type='greeting'
+        title='Welcome'
+        user = {loggedIn?.firstName || 'Guest'}
+        subtext='Access and Manage your Account and Transactions efficiently.'
+
+
+
+        
+        
+        />
+
+        <TotalBalanceBox
+        accounts={[]}
+        totalBanks={1}
+        totalCurrentBalance={1250.35}
+        />
+      </header>
+      </div>
+     
+
+
+    </section>
   )
 }
 
