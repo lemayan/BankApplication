b7731467457ef8c52b3c089c094fb57809c3e54a
lemayan <lemayanleleina@gmail.com>
2025-06-04 12:20:05 +0300

    Enhances Sidebar with dynamic links and active state styling
    Refactors the Sidebar component to include dynamic navigation links sourced from constants. Introduces active state styling based on the current pathname, improving user navigation experience. Updates class names and styling for consistency and readability.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,29 +1,56 @@
-import Link from "next/link"
-import Image from "next/image";
+'use client'
 
-const Sidebar = ({user}: SiderbarProps) => {
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
+
   return (
-    <section className='sidebar'>
-        <nav className='flex flex-col gap-4'>
-            <Link  href='/' className='mb-12 cursor-pointer items-center gap-2' >  
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+          <Image 
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Horizon logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Ivy Banking</h1>
+        </Link>
 
+        {sidebarLinks.map((item) => {
+          const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
 
-             <Image
-                src="icons/logo.svg"
-                alt="IvyBanking Logo"
-                width={34}
-                height={34}
-                className="size-[24px] max-xl: size-14"
-                
-
-            />
-            <h1 className="sidebar-logo">IvyBanking</h1>
-                 
- 
+          return (
+            <Link href={item.route} key={item.label}
+              className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}
+            >
+              <div className="relative size-6">
+                <Image 
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({
+                    'brightness-[3] invert-0': isActive
+                  })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
             </Link>
+          )
+        })}
+        
+        
+      </nav>
 
-
-        </nav>
 
     </section>
   )
