872ed995127e7377e109fba60bbb58b8e0fb1744
lemayan <lemayanleleina@gmail.com>
2025-06-10 10:08:14 +0300

    Add RightSidebar component and enhance Home page with user details and recent transactions. && Implement the complete layout
    Add RightSidebar and enhance Home and MobileNav layouts
    
    Integrates the RightSidebar component into the Home page, displaying
    user details, recent transactions, and bank balances. Updates the
    MobileNav layout for improved structure and styling consistency.
    Supports a more complete and user-friendly application interface.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = {firstName: 'Ivy'}
+  const loggedIn = {firstName: 'Ivy', lastName:'Mwikali', email: "ivymwikali@gmail.com"}
   return (
     <section className='home'>
 
@@ -22,7 +23,16 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar 
+      user ={loggedIn}
+      transactions={[]}
+      banks={[{currentBalance : 230.50}, { currentBalance: 500.50}]}
+      
+      
+      
+      />
      
 
 
