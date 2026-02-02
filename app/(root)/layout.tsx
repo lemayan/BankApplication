743b775fcb1479db097185dbd3ce023d599fd53d
Lemayan Leleina <lemayanleleina@gmail.com>
2026-02-02 12:01:04 +0300

    refactor: clean up imports, update PostCSS config, and enhance TypeScript settings

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,9 @@
 import Sidebar from "@/components/Sidebar";
-import { Children, Suspense } from "react";
+import { Suspense } from "react";
 import Image from "next/image";
 import MobileNav from "@/components/MobileNav";
-import { get } from "http";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
-import { redirect, useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 
 export default async function RootLayout({
